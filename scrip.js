a5f47e0f9259e59c3396b1adfde06561a565fb15
tipalsyahhh <tipalsyah123@gmail.com>
2024-04-22 06:17:46 +0700

    pantek

--- scrip.js
+++ scrip.js
@@ -5,12 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const nextButtonInitial = document.querySelector('.button-next');
     const button2 = document.querySelector('.button-2 button');
     const content3 = document.querySelector('.conten-3');
-    
-    // Sembunyikan elemen-elemen kecuali .home saat halaman dimuat
-    content2.style.display = 'none';
     content1Initial.style.display = 'none';
-    content3.style.display = 'none'; // Mulai dengan menyembunyikan .conten-3 juga
-    
+    content2.style.display = 'none';
+    content3.style.display = 'none';
+
     nextButton.addEventListener('click', function() {
         const content1 = document.querySelector('.conten-1');
         if (content1.style.display !== 'none') {
@@ -21,18 +19,17 @@ document.addEventListener('DOMContentLoaded', function() {
             content2.style.display = 'none';
         }
     });
-    
+
     nextButtonInitial.addEventListener('click', function() {
         const home = document.querySelector('.home');
         home.style.display = 'none';
         content1Initial.style.display = 'block';
-        content2.style.display = 'none'; // Pastikan content2 disembunyikan kembali
-        content3.style.display = 'none'; // Pastikan content3 disembunyikan kembali
+        content2.style.display = 'none';
+        content3.style.display = 'none';
     });
-    
+
     button2.addEventListener('click', function() {
         content2.style.display = 'none';
-        // Pastikan untuk menampilkan .conten-3 dengan mengubah style.display menjadi 'block'
         content3.style.display = 'block';
     });  
 
