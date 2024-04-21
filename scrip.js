477713eb0ac5fc167cf45d200ee271a87a79a2da
tipalsyahhh <tipalsyah123@gmail.com>
2024-04-22 04:06:54 +0700

    pantek

--- scrip.js
+++ scrip.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nextButton = document.querySelector('.button-1 button');
     const content2 = document.querySelector('.conten-2');
+    const content1Initial = document.querySelector('.conten-1');
+    const nextButtonInitial = document.querySelector('.button-next');
+    const button2 = document.querySelector('.button-2 button');
+    const content3 = document.querySelector('.conten-3');
+    
+    // Sembunyikan elemen-elemen kecuali .home saat halaman dimuat
     content2.style.display = 'none';
+    content1Initial.style.display = 'none';
+    content3.style.display = 'none'; // Mulai dengan menyembunyikan .conten-3 juga
+    
     nextButton.addEventListener('click', function() {
         const content1 = document.querySelector('.conten-1');
         if (content1.style.display !== 'none') {
@@ -12,15 +21,21 @@ document.addEventListener('DOMContentLoaded', function() {
             content2.style.display = 'none';
         }
     });
-    const content1Initial = document.querySelector('.conten-1');
-    content1Initial.style.display = 'none';
-    const nextButtonInitial = document.querySelector('.button-next');
+    
     nextButtonInitial.addEventListener('click', function() {
         const home = document.querySelector('.home');
         home.style.display = 'none';
         content1Initial.style.display = 'block';
+        content2.style.display = 'none'; // Pastikan content2 disembunyikan kembali
+        content3.style.display = 'none'; // Pastikan content3 disembunyikan kembali
     });
     
+    button2.addEventListener('click', function() {
+        content2.style.display = 'none';
+        // Pastikan untuk menampilkan .conten-3 dengan mengubah style.display menjadi 'block'
+        content3.style.display = 'block';
+    });  
+
     const backgroundMusic = document.getElementById('backgroundMusic');
     function playBackgroundMusic() {
         if (backgroundMusic.paused) {
